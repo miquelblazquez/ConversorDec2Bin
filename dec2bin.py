c46c9d6c0553fd9410a3fa0713d5d85838944c67
miquelblazquez <al415888@uji.es>
2021-09-15 16:41:05 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -10,12 +10,12 @@ def dec2bin(numero_decimal, numero_bits):
     if numero_decimal >= 0:
             numero_binario = numero_binario[2:len(numero_binario)]  # quita el "0b" del principio
     
-    while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
-        numero_binario = "0" + numero_binario
-        else:
-            numero_binario = numero_binario[3:len(numero_binario)]  # quita el "-0b" del principio 
-            while len(numero_binario) < numero_bits:    # añade 1's a la izquierda si hace falta 
-                numero_binario = "1" + numero_binario
+        while len(numero_binario) < numero_bits:      # añade 0's a la izquierda si hace falta
+            numero_binario = "0" + numero_binario
+    else:
+        numero_binario = numero_binario[3:len(numero_binario)]  # quita el "-0b" del principio 
+        while len(numero_binario) < numero_bits:    # añade 1's a la izquierda si hace falta 
+        numero_binario = "1" + numero_binario
     return numero_binario
 
 # ----------------------------------------
